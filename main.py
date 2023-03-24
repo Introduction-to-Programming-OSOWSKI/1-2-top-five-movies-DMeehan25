75b93284ac90ab00a315fed384277a88a704d858
DMeehan25 <833056@mystma.org>
2023-03-24 14:33:07 -0500

    2

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 def topFiveMovies( a, b, c, d, e ):
-    return( " 5 Ready Player One ", " 4 everything everywhere all at once ", " 3 Pacific Rim ", " 2 Puss and Boots " , "1 Chicken Little")
+    return( a + "" + b + "" + "" + c + "" + d + "" + e)
+
